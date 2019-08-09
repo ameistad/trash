01d10d57918bf496bbd5f32b35b85473b0605766
ameistad <ameistad@gmail.com>
2019-08-09 23:19:41 +0200

    Add test for dedup_vec

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,15 @@ pub fn dedup_vec<T>(vs: &Vec<T>) -> Vec<T> where T: std::clone::Clone + std::cmp
         .into_iter()
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dedup_vec() {
+        let vec1 = dedup_vec(&vec![1, 1, 2, 3]);
+        let vec2 = dedup_vec(&vec![1, 2, 3]);
+        assert_eq!(vec1.len(), vec2.len());
+    }
+}
